c7e41cd0bbd8733b7f7658dbaf6197c7ab48374a
Maxime Aubaret <maxime.aubaret@helileo.com>
2014-06-11 16:10:02 +0200

    tp2 exo1: second commit

--- tp2/exo1/exo1.tcl
+++ tp2/exo1/exo1.tcl
@@ -4,8 +4,15 @@ set nf [open out.nam w]
 $ns namtrace-all $nf
 
 proc finish {} {
-  global ns nf
+  global ns nf qEC qCS
   $ns flush-trace
+
+  # print useful infos
+  $qEC printPolicyTable
+  $qEC printPolicerTable
+  $qCS printPHBTable
+  $qEC printStats
+
   # fermer le fichier trace
   close $nf
   # exécuter le nam avec en entrée le fichier trace
@@ -16,7 +23,7 @@ proc finish {} {
 # Variables
 set cir1 1000000
 set cir2 500000
-set CBS 500
+set cbs 500
 
 set T_min_RED1 4
 set T_max_RED1 10
@@ -40,8 +47,15 @@ set s [$ns node] ;# Server
 # Création des liens
 $ns simplex-link $c1 $e 1.5Mb 2ms DropTail
 $ns simplex-link $c2 $e 1.5Mb 2ms DropTail
+# $ns simplex-link $e $c 1.5Mb 2ms DropTail
+# $ns simplex-link $c $s 1.5Mb 2ms DropTail
 $ns simplex-link $e $c 1.5Mb 2ms dsRED/edge
 $ns simplex-link $c $s 1.5Mb 2ms dsRED/core
+
+$ns simplex-link-op $c1 $e orient right-down
+$ns simplex-link-op $c2 $e orient right-up
+$ns simplex-link-op $e $c orient right
+$ns simplex-link-op $c $s orient right
 
 # Agents
 set agent_udp_c1 [new Agent/UDP]
@@ -49,7 +63,7 @@ $agent_udp_c1 set class_ 1
 $ns attach-agent $c1 $agent_udp_c1
 
 set agent_udp_c2 [new Agent/UDP]
-$agent_udp_c1 set class_ 2
+$agent_udp_c2 set class_ 2
 $ns attach-agent $c2 $agent_udp_c2
 
 set agent_null_s_c1 [new Agent/Null]
@@ -63,22 +77,52 @@ $ns connect $agent_udp_c2 $agent_null_s_c2
 # Sources de trafic
 set cbr_c1 [new Application/Traffic/CBR]
 $cbr_c1 attach-agent $agent_udp_c1
-$cbr_c1 set packetSize_ [expr $cir1 + 10000]
+$cbr_c1 set rate_ [expr $cir1 + 10000]
 
 set cbr_c2 [new Application/Traffic/CBR]
 $cbr_c2 attach-agent $agent_udp_c2
-$cbr_c2 set packetSize_ [expr $cir2 + 10000]
+$cbr_c2 set rate_ [expr $cir2 + 10000]
 
 # Configuration DiffServ
 set qEC [[$ns link $e $c] queue]
 
-$qEC meanPktSize 100 ;# Taille moyenne des paquets
+$qEC meanPktSize 210 ;# Taille moyenne des paquets
+
 $qEC set numQueues_ 1 ;# Buffer physique
 $qEC setNumPrec 2 ;# Nombre Buffer virtuels
 
-$qEC addPolicyEntry [$c1 id] [$s id] TokenBucket DSCP $cir0 $cbs
-$qEC addPolicyEntry [$c2 id] [$s id] TokenBucket DSCP $cir1 $cbs
-$qEC addPolicerEntry TokenBucket DSCP DSCP2
+$qEC addPolicyEntry [$c1 id] [$s id] TokenBucket 10 $cir1 $cbs
+$qEC addPolicyEntry [$c2 id] [$s id] Null 20 ;# $cir2 $cbs
 
-$qEC configQ 0 0 $T_min_RED0 $T_max_RED0 $Proba_Perte0
-$qEC configQ 0 1 $T_min_RED1 $T_max_RED1 $Proba_Perte1
+$qEC addPolicerEntry TokenBucket 10 11
+$qEC addPolicerEntry Null 20
+
+$qEC addPHBEntry 10 0 0
+$qEC addPHBEntry 11 0 1
+$qEC addPHBEntry 20 0 1
+
+$qEC configQ 0 0 $T_min_RED1 $T_max_RED1 $Proba_Perte1
+$qEC configQ 0 1 $T_min_RED2 $T_max_RED2 $Proba_Perte2
+
+# CS
+set qCS [[$ns link $c $s] queue]
+
+$qCS meanPktSize 210
+$qCS set numQueues_ 1
+$qCS setNumPrec 2
+
+$qCS addPHBEntry 10 0 0
+$qCS addPHBEntry 11 0 1
+$qCS addPHBEntry 20 0 1
+
+$qCS configQ 0 0 $T_min_RED1 $T_max_RED1 $Proba_Perte1
+$qCS configQ 0 1 $T_min_RED2 $T_max_RED2 $Proba_Perte2
+
+# Scenario
+$ns at 0.0 "$cbr_c1 start"
+$ns at 0.0 "$cbr_c2 start"
+$ns at 4.0 "$cbr_c1 stop"
+$ns at 4.0 "$cbr_c2 stop"
+$ns at 5.0 "finish"
+
+$ns run
